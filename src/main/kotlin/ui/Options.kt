0d4c19ad3ac6a0aef7531111901d1f2b951e8be8
Daniel Seiler <73987329+daniel-seiler@users.noreply.github.com>
2021-09-06 13:02:03 +0200

    added enum to check all possible options for the user

---
+++ src/main/kotlin/ui/Options.kt
@@ -0,0 +1,13 @@
+package ui
+
+enum class Options(r: Regex) {
+    INPATH("".toRegex()),
+
+    OUTPATH("".toRegex()),
+
+    URL("".toRegex()),
+
+    GRAY("".toRegex()),
+
+    HELP("".toRegex())
+}
