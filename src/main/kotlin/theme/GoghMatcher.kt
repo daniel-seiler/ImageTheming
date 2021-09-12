c3a3960d3c29fad5f4e1876da402e82cee400948
Daniel Seiler <73987329+daniel-seiler@users.noreply.github.com>
2021-09-12 14:09:17 +0200

    added comments to gogh matcher

--- src/main/kotlin/theme/GoghMatcher.kt
+++ src/main/kotlin/theme/GoghMatcher.kt
@@ -5,10 +5,21 @@ import java.lang.Integer.parseInt
 import java.net.URL
 import java.nio.charset.Charset
 
+/**
+ * Load a color scheme from Gogh. Uses regex for future use with config files.
+ *
+ * @property name   name of theme file without .sh
+ */
 class GoghMatcher(val name: String) {
     private val regex: Regex = "COLOR(?:_\\d{2})?=\"#(?<color>[\\dabcdefABCDEF]{6})\"".toRegex()
-    private val theme = URL("https://raw.githubusercontent.com/Mayccoll/Gogh/master/themes/${name}.sh").readText(Charset.defaultCharset())
+    private val theme = URL("https://raw.githubusercontent.com/Mayccoll/Gogh/master/themes/${name}.sh")
+        .readText(Charset.defaultCharset())
 
+    /**
+     * Find all colors from the config file and write them to a string list.
+     *
+     * @return      list of all colors
+     */
     private fun match() : List<String> {
         val matches = regex.findAll(theme)
         var outputList = ArrayList<String>()
@@ -16,6 +27,11 @@ class GoghMatcher(val name: String) {
         return outputList
     }
 
+    /**
+     * Add all colors to the theme class.
+     *
+     * @return      a new theme class with the matched colors
+     */
     fun getTheme() : Theme {
         val stringList = match()
         val colorList = ArrayList<Color>()
